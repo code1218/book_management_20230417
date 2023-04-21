3bbf138ef065a0744b48b50cf6cd2588d98567fe
code1218 <skjil1218@gmail.com>
2023-04-21 11:51:04 +0900

    로그인 유지 완료

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Main from './pages/Main/Main';
 import AuthRoute from './components/Routes/AuthRoute/AuthRoute';
-import { useRecoilValue } from 'recoil';
-import { authenticated } from './index';
 
 function App() {
   return (
     <>
       <Global styles={ Reset }></Global>
       <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={
-          <AuthRoute authenticated={useRecoilValue(authenticated)} element={<Main />} />
-          } />
-
+        <Route exact path="/login" element={ <AuthRoute path="/login" element={<Login />} /> } />
+        <Route path="/register" element={ <AuthRoute path="/register" element={<Register />} /> } />
+        <Route path="/" element={ <AuthRoute path="/" element={<Main />} /> } />
       </Routes>
     </>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,7 +7,7 @@ import { FiUser, FiLock } from 'react-icons/fi';
 import { BsGoogle } from 'react-icons/bs';
 import { SiNaver, SiKakao } from 'react-icons/si';
 import axios from 'axios';
-import { authenticated } from '../../index';
+import { authenticatedState } from '../../atoms/Auth/AuthAtoms';
 import { useRecoilState } from 'recoil';
 
 const container = css`
@@ -116,7 +116,8 @@ const errorMsg = css`
 const Login = () => {
     const [ loginUser, setLoginUser ] = useState({email: "", password: ""});
     const [ errorMessages, setErrorMessages ] = useState({email: "", password: ""});
-    const [ auth, setAuth ] = useRecoilState(authenticated);
+    const [ authenticated, setAuthenticated ] = useRecoilState(authenticatedState);
+
     const navigate = useNavigate();
 
     const handleChange = (e) => {
@@ -135,7 +136,7 @@ const Login = () => {
             setErrorMessages({email: "", password: ""});
             const accessToken = response.data.grantType + " " + response.data.accessToken;
             localStorage.setItem("accessToken", accessToken);
-            setAuth(true);
+            setAuthenticated(true);
             navigate("/");
         }catch(error) {
             setErrorMessages({email: "", password: "", ...error.response.data.errorData});
